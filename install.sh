be8d020c4602843f78eca28572ad581e5b8ab4d2
Maarten Slagter <slagter.maarten@gmail.com>
2014-10-26 10:44:44 +0100

    Improved install.sh, it now backs up old dotfiles

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+echo "Moving any existing dotfiles from ~ to $olddir ... "
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    [[ -e ~/.$file ]] && mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
