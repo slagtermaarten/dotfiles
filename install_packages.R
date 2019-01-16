473a756250f02b7fa6bda569ba6ce97333f1ff65
Maarten Slagter <slagter.maarten@gmail.com>
2019-01-16 18:48:09 +0100

    Better install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,6 +1,39 @@
-if (!require('devtools')) { install.packages('devtools') }
-if (!require('data.table')) { devtools::install_github('Rdatatable/data.table') }
-if (!require('vimcom')) { devtools::install_github('jalvesaq/VimCom') }
-if (!require('pacman')) { install.packages('pacman') }
-if (!require('colorout')) { devtools::install_github('jalvesaq/colorout') }
-if (!require('maartenutils')) { devtools::install('~/bin/maartenutils') }
+#!/usr/bin/env r
+
+if (!require('devtools')) { 
+  tryCatch(install.packages('devtools'), 
+    error = function(e) { print(e) })  
+}
+if (!require('data.table')) { 
+  tryCatch(devtools::install_github('Rdatatable/data.table'), 
+    error = function(e) { print(e) })  
+}
+# if (!require('vimcom')) { 
+#   tryCatch(devtools::install_github('jalvesaq/VimCom'), 
+#     error = function(e) { print(e) })  
+# }
+if (!require('pacman')) { 
+  tryCatch(install.packages('pacman'), 
+    error = function(e) { print(e) })  
+}
+if (!require('colorout')) { 
+  tryCatch(devtools::install_github('jalvesaq/colorout'), 
+    error = function(e) { print(e) })  
+}
+if (!require('maartenutils')) { 
+  tryCatch(devtools::install('~/libs/maartenutils'), 
+    error = function(e) { 
+      tryCatch(devtools::install_github('slagtermaarten/maartenutils'), 
+        error = function(e) { 
+          print(e) 
+        })  
+    })  
+}
+if (!require('docopt')) { 
+  tryCatch(devtools::install_github('docopt/docopt.R'), 
+    error = function(e) { print(e) })  
+}
+if (!require('tidyverse')) { 
+  tryCatch(install.packages('tidyverse'), 
+    error = function(e) { print(e) })  
+}
